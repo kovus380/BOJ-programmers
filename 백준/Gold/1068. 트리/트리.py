3138a8128d7be5d373d1c39c8d9ea338f59edc62
Chae-Yeon OK <86646679+kovus380@users.noreply.github.com>
2023-08-05 16:26:44 +0900

    [Gold V] Title: 트리, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/1068. 트리/트리.py
+++ 백준/Gold/1068. 트리/트리.py
@@ -1,33 +1,21 @@
-from collections import defaultdict, deque
-
 n = int(input())
 parents = list(map(int, input().split()))
 rem_node = int(input())
 
-root = 0
-nodes = defaultdict(list)
+def dfs(delete, parents):
+    parents[delete] = -2
 
-for idx, parent in enumerate(parents):
-    if parent == -1:
-        root = idx
-        continue
-    nodes[parent] += [idx]
+    for i in range(n):
+        if (parents[i] == delete):
+            dfs(i, parents)
+    
 
-need_visited = deque([rem_node])
+dfs(rem_node, parents)
 
-while need_visited:
-    v = need_visited.popleft()
-    need_visited += nodes[v]
-    nodes.pop(v)
+leaf_cnt = 0
 
-answer = 0
+for i in range(n):
+    if (parents[i] != -2) and (i not in parents):
+        leaf_cnt += 1
 
-need_visited = deque([[root]]  if nodes else [])
-while need_visited:
-    visited = need_visited.popleft()
-    if visited[-1] not in nodes or nodes[visited[-1]] == [rem_node]:
-        answer += 1
-    else:
-        need_visited += [ visited + [n] for n in nodes[visited[-1]] if not n == rem_node]
-
-print(answer)
+print(leaf_cnt)
