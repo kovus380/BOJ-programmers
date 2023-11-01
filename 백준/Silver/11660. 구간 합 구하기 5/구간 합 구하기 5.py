201540efb91be29cd2025e9a900db1e7f638165c
Chae-Yeon OK <86646679+kovus380@users.noreply.github.com>
2023-11-01 09:14:03 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1004 ms, Memory: 105932 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -2,20 +2,20 @@ import sys
 
 input = sys.stdin.readline
 
-
 n, m = map(int, input().split())
 
-nums = []
-for _ in range(n):
-    nums.append(list(map(int, input().split())))
+grid = [list(map(int, input().split())) for _ in range(n)]
 
-dp = [[0] * (n + 1) for _ in range(n + 1)]
+sums = [[0] * (n + 1) for _ in range(n + 1)]
 
-for i in range(1, n + 1):
-    for j in range(1, n + 1):
-        dp[i][j] = dp[i - 1][j] + dp[i][j - 1] - dp[i -1][j -1] + nums[i - 1][j - 1]
+for i in range(n):
+    temp = [0]
+    for j in range(n):
+        temp.append(temp[-1] + grid[i][j])
+        sums[i+1][j+1] = sums[i][j+1] + temp[-1]
 
 for _ in range(m):
     x1, y1, x2, y2 = map(int, input().split())
-    answer = dp[x2][y2] - dp[x1 - 1][y2] - dp[x2][y1 - 1] + dp[x1 - 1][y1 -1]
-    print(answer)
+    print(sums[x2][y2] - sums[x1-1][y2] - sums[x2][y1-1] + sums[x1-1][y1-1])
+
+
