36b9ab78eca9c399e9e7b65d8e47fde75949467e
Chae-Yeon OK <86646679+kovus380@users.noreply.github.com>
2023-11-24 00:43:13 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 4096 ms, Memory: 56384 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -7,24 +7,25 @@ edges.sort(key=lambda x:x[2])
 
 answer_edges = 0
 
-def find(v):
-    if parents[v] == v:
-        return v
-    else:
-        parents[v] = find(parents[v])
-        return parents[v]
-    
+
+def find_parent(v):
+    if parents[v] != v:
+        parents[v] = find_parent(parents[v])
+    return parents[v]
+
 def union(v1, v2):
-    v1 = find(v1)
-    v2 = find(v2)
-    
-    if v1 < v2:
-        parents[v2] = v1
-    else:
+    v1 = find_parent(v1)
+    v2 = find_parent(v2)
+
+    if v1 > v2:
         parents[v1] = v2
+    else:
+        parents[v2] = v1
+
+
 
 for v1, v2, w in edges:
-    if find(parents[v1]) != find(parents[v2]):
+    if find_parent(v1) != find_parent(v2):
         answer_edges += w
         union(v1, v2)
 
