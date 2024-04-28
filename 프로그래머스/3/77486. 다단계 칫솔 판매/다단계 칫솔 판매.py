7ae49462e3eaef14c8ab1fce4b9f6192a70e5e3a
Chae-Yeon OK <86646679+kovus380@users.noreply.github.com>
2024-04-28 22:27:13 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 208.40 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.py
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.py
@@ -1,31 +1,3 @@
-from collections import defaultdict
-
 def solution(enroll, referral, seller, amount):
     answer = []
-    
-    ref = defaultdict(str)
-    profit = defaultdict(int)
-    
-        
-    for idx in range(len(referral)):
-        ref[enroll[idx]] = referral[idx]
-    
-    have_to_visit = [[seller[idx], a * 100] for idx, a in enumerate(amount)]
-    
-        
-    while have_to_visit:
-        
-        name, won = have_to_visit.pop()
-        
-        if  won < 10:
-            profit[name] += won
-            continue
-            
-        else:
-            profit[name] += won - int(won * 0.1)
-        if name == '':
-            continue
-        
-        have_to_visit.append([ref[name], int(won * 0.1)])
-        
-    return [profit[e] for e in enroll]
+    return answer
